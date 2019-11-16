d2c22487ae0800ff64b745572b7c6529227f7877
“sangameshrently” <sangamesh.r@rently.com>
2019-11-15 23:48:29 -0800

    working react-native-location code.

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,14 +1,56 @@
 
 
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableHighlight } from 'react-native';
+import RNLocation from 'react-native-location';
+
+RNLocation.configure({
+    distanceFilter: 5.0
+})
+
 
 class HomeScreenComponent extends React.Component {
+
     render = () => {
         return (
             <View>
                 <Text> This will be HomeScreen screen </Text>
+
+                <TouchableHighlight onPress={() => {
+                    RNLocation.requestPermission({
+                        ios: "whenInUse",
+                        android: {
+                            detail: "coarse"
+                        }
+                    }).then(granted => {
+                        if (granted) {
+                            this.locationSubscription = RNLocation.subscribeToLocationUpdates(locations => {
+                                console.log('locations', locations);
+
+                                /* Example location returned
+                                {
+                                  speed: -1,
+                                  longitude: -0.1337,
+                                  latitude: 51.50998,
+                                  accuracy: 5,
+                                  heading: -1,
+                                  altitude: 0,
+                                  altitudeAccuracy: -1
+                                  floor: 0
+                                  timestamp: 1446007304457.029,
+                                  fromMockProvider: false
+                                }
+                                */
+                            })
+                        }
+                    })
+                }}>
+                    <Text>I am in trouble</Text>
+
+                </TouchableHighlight>
             </View>
+
+
         )
     }
 }
